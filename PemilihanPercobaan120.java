6bfbacfea9f353faace1d30b129d237549463d3d
abhixyz1 <irfanhendra90@gmail.com>
2023-10-10 22:26:35 +0700

    tugas jobsheet 5

---
+++ PemilihanPercobaan120.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class PemilihanPercobaan120{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan Sebuah Angka");
+        int angka = sc.nextInt();
+        String result = (angka % 2 == 0) ? "genap" : "ganjil";
+        System.out.println("Angka " + angka + " termasuk bilangan " + result);
+
+    }
+}
