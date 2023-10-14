b443be2d71373246233b0ef31f431d0bdb329420
abhixyz1 <irfanhendra90@gmail.com>
2023-10-14 23:01:09 +0700

    percobaan praktikum daspro ke 5

--- PemilihanPercobaan220.java
+++ PemilihanPercobaan220.java
@@ -4,30 +4,33 @@ class PemilihanPercobaan220  {
     public static void main(String[] args) {
         Scanner input20 = new Scanner(System.in);
 
-        System.out.println("Nilai UAS : ");
-        float UAS = input20.nextFloat();
-        System.out.println("Nilai UTS : ");
-        float UTS = input20.nextFloat();
-        System.out.println("Nilai Kuis : ");
-        float Kuis = input20.nextFloat();
-        System.out.println("Nilai Tugas : ");
-        float Tugas = input20.nextFloat();
+        double angka1, angka2, hasil;
+        char operator;
+        
+        System.out.println("Masukkan angka pertama: ");
+        angka1 = input20.nextDouble();
+        System.out.println("Masukkan angka kedua: ");
+        angka2 = input20.nextDouble();
+        System.out.println("Masukkan operator (+ - * /): ");
+        operator = input20.next().charAt(0);
 
-        float total = (UAS * 0.4F) + (UTS * 0.3F) + (Kuis * 0.1F) + (Tugas * 0.2F);
-
-        if (total > 80)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan A ");
-        else if (total > 73)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan B+");
-        else if (total > 65)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan B");
-        else if (total > 60)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan C+");
-        else if (total > 50)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan C");
-        else if (total > 39)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan D");
-        else
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan E");
+        switch (operator) {
+            case '+' :
+            hasil = angka1 + angka2;
+            System.out.println(angka1 + " + " + angka2 + "=" + hasil);
+            break;
+            case '-' :
+            hasil = angka1 - angka2;
+            System.out.println(angka1 + " - " + angka2 + "=" + hasil);
+            break;
+            case '*' :
+            hasil = angka1 * angka2;
+            System.out.println(angka1 + " * " + angka2 + "=" + hasil);
+            break;
+            case '/' :
+            hasil = angka1 / angka2;
+            System.out.println(angka1 + " / " + angka2 + "=" + hasil);
+            break;
+        }
     }
 }
