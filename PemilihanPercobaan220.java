558058cc0cb40012c7a21c901a962805ffb1d8d9
abhixyz1 <irfanhendra90@gmail.com>
2023-10-20 11:05:17 +0700

    percobaan 2

--- PemilihanPercobaan220.java
+++ PemilihanPercobaan220.java
@@ -14,20 +14,21 @@ class PemilihanPercobaan220  {
         float Tugas = input20.nextFloat();
 
         float total = (UAS * 0.4F) + (UTS * 0.3F) + (Kuis * 0.1F) + (Tugas * 0.2F);
+        String message = total < 65 ? "Remidi" : "Tidak Remidi";
 
         if (total > 80)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan A ");
+        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan A dan " + message);
         else if (total > 73)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan B+");
+        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan B+ dan "+message);
         else if (total > 65)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan B");
+        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan B dan "+message);
         else if (total > 60)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan C+");
+        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan C+ dan "+message);
         else if (total > 50)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan C");
+        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan C dan "+message);
         else if (total > 39)
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan D");
+        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan D dan "+message);
         else
-        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan E");
+        System.out.println("Nilai akhir = " + total + " sehingga mendapatkan E dan "+message);
     }
 }
